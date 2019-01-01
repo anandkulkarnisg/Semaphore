0534a11bd55bb6b67b020608fc6d98dc725185c0
Anand Kulkarni <anand.kulkarni.sg@gmail.com>
2019-01-01 20:13:58 +0800

    Anand Kulkarni - 01/01/2019 20:13:56:126 Asia/Singapore(+08,+0800) - Added some important indentation..

--- Example6.cpp
+++ Example6.cpp
@@ -5,31 +5,31 @@
 
 using namespace std;
 
-Semaphore binarySemLock(1,true,true);
-const int demoLoopCount = 100;
+Semaphore binarySemLock(1, true, true);
+const int demoLoopCount=100;
 
 void printMessage()
 {
-	int loopCount=0;
-	while(loopCount<demoLoopCount)
-	{
-		binarySemLock.acquire();
-		cout << "I am currently in thread id =" << this_thread::get_id() << " with a count = " << loopCount << endl;
-		binarySemLock.release();
-		++loopCount;
-	}
+  int loopCount=0;
+  while(loopCount<demoLoopCount)
+  {
+    binarySemLock.acquire();
+    cout << "I am currently in thread id =" << this_thread::get_id() << " with a count = " << loopCount << endl;
+    binarySemLock.release();
+    ++loopCount;
+  }
 }
 
 int main(int argc, char* argv[])
 {
-	// Implement a binary semaphore which acts like a exclusive lock.
-	// Each thread will print its line alternatively after the other without overwriting as semaphore acts perfectly like a Lock.
+  // Implement a binary semaphore which acts like a exclusive lock.
+  // Each thread will print its line alternatively after the other without overwriting as semaphore acts perfectly like a Lock.
 
-	thread t1(&printMessage);
-	thread t2(&printMessage);
+  thread t1(&printMessage);
+  thread t2(&printMessage);
 
-	t1.join();
-	t2.join();
-	
-	return(0);
+  t1.join();
+  t2.join();
+
+  return(0);
 }
