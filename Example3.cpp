1377cd98925bff1b6e847b1c1edbce285a0d11c4
anandkulkarnisg <anand.kulkarni.sg@gmail.com>
2019-12-08 13:47:38 +0800

    Anand Kulkarni - 08/12/2019 13:47:38:113 Asia/Singapore(+08,+0800) - Added some documentation to the Example3.cpp.

--- Example3.cpp
+++ Example3.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-Semaphore test(10,true,true);
+Semaphore test(10, true, true);
 mutex cout_mutex;
 
 void printOut(const string& str1, const string& str2, const thread::id& id)
@@ -52,16 +52,17 @@ void waitThreadFunc(const int& permits)
 }
 
 // This code example may get blocked at t1 thread indefinitely. This is not a bug. because thread t2 and t3 jump ahead and acquire 2,3 permits and only 5 are left from 10.
-// Next t1 attempts to acquire 10 permits but only 5 are present.so it is blocked!! This is bug at application layer.
+// Next t1 attempts to acquire 10 permits but only 5 are present.so it is blocked!! This is bug at application layer. It demonstrates that thread safe data structures can
+// still be mis used at application level.
 
 int main(int argc, char* argv[])
 {
 	// Test Case 1 : Spin a thread that will acquire all the permits and wait for given period of time and then release the permit.
-	thread t1(&acquireThreadFunc,10,5);
+	thread t1(&acquireThreadFunc, 10, 5);
 
 	// Spin two threads that will wait for the permits to become available and then release them one by one in the first thread.
-	thread t2(&waitThreadFunc,2);	
-	thread t3(&waitThreadFunc,3);
+	thread t2(&waitThreadFunc, 2);	
+	thread t3(&waitThreadFunc, 3);
 
 	// Test that the threads have queued. Verify their order of release.
 	this_thread::sleep_for(chrono::seconds(2));
