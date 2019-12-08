6c87471633c1a93d919117ce945e1f3e91c4a4c1
anandkulkarnisg <anand.kulkarni.sg@gmail.com>
2019-12-08 12:24:34 +0800

    Anand Kulkarni - 08/12/2019 12:24:33:791 Asia/Singapore(+08,+0800) - formatted the exceptions code..

--- SemaphoreExceptions.h
+++ SemaphoreExceptions.h
@@ -6,7 +6,6 @@
 #define SemaphoreExceptions_H
 
 const char* IllegalArgumentException_msg = "Exception : Initialization not allowed with -ve permits in strict mode. Use relaxed mode instead.";
-
 struct IllegalArgumentException : public std::exception
 {
 	const char * what () const throw ()
@@ -15,9 +14,7 @@ struct IllegalArgumentException : public std::exception
 	}
 };
 
-
 const char* NegativeReleasePermitsException_msg = "Exception : release method can not have -ve permits. Please check the value.";
-
 struct NegativeReleasePermitsException : public std::exception
 {
 	const char * what () const throw ()
@@ -26,9 +23,7 @@ struct NegativeReleasePermitsException : public std::exception
 	}
 };
 
-
 const char* ExcessReleasePermitsException_msg = "Exception : An attempt was made to release permits greater than the current number of permits [in strict mode] held by the Thread id =";
-
 struct ExcessReleasePermitsException : public std::exception
 {
 	const char * what () const throw ()
@@ -37,9 +32,7 @@ struct ExcessReleasePermitsException : public std::exception
 	}
 };
 
-
 const char* IllegalPermitsReleaseException_msg = "Exception : An attempt was made to release permits in strict mode where it has no currently acquired permits.Please investigate. Thread id =";
-
 struct IllegalPermitsReleaseException : public std::exception
 {
 	const char * what () const throw ()
@@ -47,6 +40,5 @@ struct IllegalPermitsReleaseException : public std::exception
 		return(IllegalPermitsReleaseException_msg);
 	}
 };
-
 
 #endif
