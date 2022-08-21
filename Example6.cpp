7584aaea28168e2dbb6ce3ca2b434f27b4d71806
anandkulkarnisg <anand.kulkarni.sg@gmail.com>
2022-08-21 15:44:22 +0800

    Anand Kulkarni - 21/08/2022 15:44:21:620 Asia/Singapore(+08,+0800) - commited via autocommit tool.

--- Example6.cpp
+++ Example6.cpp
@@ -5,10 +5,7 @@
 
 using namespace std;
 
-Semaphore binarySemLock(1, true, true);
-constexpr int demoLoopCount=100;
-
-void printMessage()
+void printMessage(Semaphore& binarySemLock, const int& demoLoopCount)
 {
   int loopCount=0;
   while(loopCount<demoLoopCount)
@@ -24,9 +21,13 @@ int main(int argc, char* argv[])
 {
   // Implement a binary semaphore which acts like a exclusive lock.
   // Each thread will print its line alternatively after the other without overwriting as semaphore acts perfectly like a Lock.
+  
+  // Create a semaphore locally here and pass to threads.
+  Semaphore binarySemLock(1, true, true);
+  constexpr int demoLoopCount=100;
 
-  thread t1(&printMessage);
-  thread t2(&printMessage);
+  thread t1(&printMessage, std::ref(binarySemLock), demoLoopCount);
+  thread t2(&printMessage, std::ref(binarySemLock), demoLoopCount);
 
   t1.join();
   t2.join();
